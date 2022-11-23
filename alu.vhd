0cbb758d8d8314a07768f71ae9ad0beb6181afac
Thales Alves <thalesalves@alunos.utfpr.edu.br>
2022-11-22 22:36:03 -0300

    test(alu): Added Numeric_Std lib alu_pkg deprecated with adition of numeric_std lib, added another output to represent if numbers are equal

--- alu.vhd
+++ alu.vhd
@@ -1,28 +1,41 @@
 LIBRARY IEEE;
 use IEEE.std_logic_1164.all;
-use work.alu_pkg.all;
+use IEEE.numeric_std.all;
 
 ENTITY alu is
 
 	port(
-			funct     : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
-			input_01  : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
-			input_02  : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
-			output_01 : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
+			funct     : IN UNSIGNED(2 DOWNTO 0);
+			input_01  : IN SIGNED(15 DOWNTO 0);
+			input_02  : IN SIGNED(15 DOWNTO 0);
+			equal     : OUT BIT;
+			output_01 : OUT SIGNED(15 DOWNTO 0)
 	);
 
 END alu;
 
 architecture logic of alu is
 
+	signal temp : SIGNED(15 DOWNTO 0);
+
 begin
 
-	output_01 <= sum(input_01, input_02) when funct = "000" else
-							 sub(input_01, input_02) when funct = "001" else
-							 input_01 AND input_02   when funct = "010" else
-							 input_01 OR input_02    when funct = "011" else
-							 NOT(input_01)           when funct = "100" else
-							 input_01 XOR input_02   when funct = "101" else
-							 unaffected;
+	temp <= input_01 + input_02   when funct = "000" else
+					input_01 - input_02   when funct = "001" else
+				  input_01 AND input_02 when funct = "010" else
+				  input_01 OR input_02  when funct = "011" else
+				  NOT(input_01)         when funct = "100" else
+				  input_01 XOR input_02 when funct = "101" else
+				  unaffected;
 
+	output_01 <= temp;
+
+	process(temp)
+  begin
+
+		if temp = "0000000000000000" then equal <= '1';
+		else equal <= '0';
+		end if;
+
+	end process;
 end architecture;
